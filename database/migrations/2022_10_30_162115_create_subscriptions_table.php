f3bf4e4028192b3897d294e4529a763cd22a22f1
ebencosoft <ebencosoft@gmail.com>
2022-11-01 10:19:06 +0000

    Add files via upload

---
+++ database/migrations/2022_10_30_162115_create_subscriptions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id'); // A subscription belongs to a user
+            $table->string('name');  // The name of the subscription
+            $table->string('braintree_id'); //id for the subscription
+            $table->string('braintree_plan'); // The name of the plan
+            $table->string('status'); // The name of the plan
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
